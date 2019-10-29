c8f52ca12c271c6362fbc2ca5286d9a8c5183b25
nornina98 <56819374+nornina98@users.noreply.github.com>
2019-10-30 04:15:41 +0800

    Updates

---
+++ precache-manifest.990bd5cf600a58b00d3214491b176a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fc603e98be59a51603828e9f34dfe6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "012825cd53570e4b9763",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "225dc98d9a1ac92995d4",
+    "url": "/robofriends/static/css/main.f27a1868.chunk.css"
+  },
+  {
+    "revision": "012825cd53570e4b9763",
+    "url": "/robofriends/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "225dc98d9a1ac92995d4",
+    "url": "/robofriends/static/js/main.99d9d572.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
