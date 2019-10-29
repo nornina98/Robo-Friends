aa264ae705b5dafbdd43d89f93bd784e39d44b51
nornina98 <56819374+nornina98@users.noreply.github.com>
2019-10-30 04:00:46 +0800

    structured and improved code base for code  quality

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css' ;
 
 class App extends Component {
@@ -26,13 +25,13 @@ class App extends Component {
     }
 
     render() {
-        const filterRobots = this.state.robots.filter(robots => {
-        return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        const {robots, searchField} = this.state;
+        const filterRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>SORRY ! THERE IS NO ROBOT IS AVAILABLE FOR RIGHT NOW</h1>
-        } else {
-            return (
+        return !robots.length ? 
+            <h1>SORRY ! THERE IS NO ROBOT IS AVAILABLE FOR RIGHT NOW</h1> :
+            (
                 <div className="tc">
                     <h1 className='f1'>ROBOTFRIENDS</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -41,9 +40,7 @@ class App extends Component {
                     </Scroll>    
                 </div>
             )
-        }
-        
+        }       
     }
-  }
 
 export default App;
