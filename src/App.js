0329c8872ce44cee99f40ff63a4e987caac9c12e
nornina98 <56819374+nornina98@users.noreply.github.com>
2019-10-29 22:18:30 +0800

    Add all robofriends files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+import './App.css' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            robots: robots,
+            searchField: '',
+        }
+      }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const filterRobots = this.state.robots.filter(robots => {
+        return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return (
+            <div className="tc">
+                <h1 className='f1'>ANDREI ROBOTITO</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filterRobots}/>
+            </div>
+        )
+    }
+  }
 
 export default App;
