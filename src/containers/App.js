46eff8aee5218f33c6334d06bdfb55fd2cceb75f
nornina98 <56819374+nornina98@users.noreply.github.com>
2019-11-02 23:51:17 +0800

    added redux async function for data API

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,43 +2,36 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 import './App.css' ;
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField // COME FROM REDUCERS.JS
+        searchField: state.searchRobots.searchField, // COME FROM REDUCERS.JS
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value)) 
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
 }}
 
 class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            robots: []
-        }
-      }
-
     componentDidMount(){    
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json())
-        .then(users => this.setState({robots: users}));
+       this.props.onRequestRobots();
     }
 
     render() { 
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filterRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        return !robots.length ? 
+        return isPending ? 
             <h1>SORRY ! THERE IS NO ROBOT IS AVAILABLE FOR RIGHT NOW</h1> :
             (
                 <div className="tc">
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers, } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
 
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
                 <Provider store={store}>
